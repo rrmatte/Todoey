c7c0f954e0a1e708d654a9e2d7bb307f211caa50
Ron R Matteson <rrmatteson@gmail.com>
2018-10-28 11:32:54 -0700

    adding persistent data to model

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -11,9 +11,14 @@ import UIKit
 class TodoListViewController: UITableViewController {
     @IBOutlet weak var ToDoItem: UILabel!
     
-   var itemArray = ["Find mike","Build Big Rock","Finish XMAS party"]
+    var itemArray = ["Find mike","Build Big Rock","Finish XMAS party"]
+    let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -54,6 +59,7 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen when the user clicks the Add Item
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray,forKey: "TodoListArray")
             self.tableView.reloadData()
         }
         alert.addTextField { (alertTextField) in
